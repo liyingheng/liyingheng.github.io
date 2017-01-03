ea345990e0719b8d37ace7c0d4643c0f8b83209e
liyingheng <liyingheng1@gmail.com>
2017-01-03 13:50:02 +0800

    Initial PWA Commit
    Initial Commit

---
+++ service-worker.js
@@ -0,0 +1,76 @@
+var cacheName="weatherPWA-step-6-1";
+var dataCacheName='weatherData-v1';
+var filesToCache = [  
+  '/',  
+  '/index.html',  
+  '/scripts/app.js',  
+  '/styles/inline.css',  
+  '/images/clear.png',  
+  '/images/cloudy-scattered-showers.png',  
+  '/images/cloudy.png',  
+  '/images/fog.png',  
+  '/images/ic_add_white_24px.svg',  
+  '/images/ic_refresh_white_24px.svg',  
+  '/images/partly-cloudy.png',  
+  '/images/rain.png',  
+  '/images/scattered-showers.png',  
+  '/images/sleet.png',  
+  '/images/snow.png',  
+  '/images/thunderstorm.png',  
+  '/images/wind.png'  
+];
+
+//Install Service Worker
+self.addEventListener('install',function(e) {
+    console.log('[ServiceWorker] install');
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            console.log('[ServiceWorker] Caching app shell');
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+//Activate Service Worker
+self.addEventListener('activate',function(e) {
+    console.log('[ServiceWorker] activate');
+    e.waitUntil(
+        caches.keys().then(function(keyList){
+            return Promise.all(keyList.map(function(key) {
+                console.log('[ServiceWorker] removing old cache');
+                if(key !== cacheName || key !== dataCacheName) {
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+});
+
+//Fetch event
+//Load app shell from cache
+self.addEventListener('fetch',function(e){
+    console.log('[ServiceWorker] fetch',e.request.url);
+    var dataUrl = 'https://publicdata-weather.firebaseio.com/';
+    if(e.request.url.indexOf(dataUrl) !== 0) {
+        //Put data handler code here
+        e.respondWith(
+            fetch(e.request)
+                .then(function(response) {
+                    return caches.open(dataCacheName).then(function(cache) {
+                        cache.put(e.request.url,response.clone());
+                        console.log('[ServiceWorker] Fetched & Cached data');
+                        return response;
+                    })
+                })
+        )
+    }
+    else {
+        e.respondWith(
+            caches.match(e.request).then(function(response){
+                return response || fetch(e.request);
+            })
+        );
+    }
+});
+
+
